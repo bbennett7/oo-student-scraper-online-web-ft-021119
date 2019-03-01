a92cb5f12939ea88fc2f0688c0a29ebadc9196f7
Bryn Bennett <bryn.bennett@gmail.com>
2019-03-01 08:05:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student = Student.new
+      new_student = Student.new(student)
     end
   end
 
