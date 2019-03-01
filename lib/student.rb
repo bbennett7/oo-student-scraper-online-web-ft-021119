cdd347582dbcb4aef9919e7ad0cfdf661a10d825
Bryn Bennett <bryn.bennett@gmail.com>
2019-03-01 08:06:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +9,22 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_attributes|
+      new_student = Student.new(student_attributes)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
