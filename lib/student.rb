df6609e84b42d409f17215d8cd66bffcf134c381
Bryn Bennett <bryn.bennett@gmail.com>
2019-03-01 07:55:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,7 @@ class Student
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="), value)}
     @@all << self 
+    @name = name 
   end
 
   def self.create_from_collection(students_array)
